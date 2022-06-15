bcb1cbf931e39f87fa8fae4ea6dad0abc2f86a5a
Devu Mani <devu.seeroo@gmail.com>
2022-06-15 23:06:59 +0530

    calendar widget removed from appbar

--- lib/app/modules/home/widgets/home_appbar.dart
+++ lib/app/modules/home/widgets/home_appbar.dart
@@ -1,5 +1,4 @@
 import 'package:alhaddad_driver/app/widgets/toolbar/custom_container_toolbar.dart';
-import 'package:alhaddad_driver/generated/locales.g.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,18 +17,13 @@ class HomeAppbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(
       () => CustomContainerToolbar(
-          showCustomBackgroundImage: showCustomBackgroundImage,
-          isHasLeading: true,
-          onPressedLeading: () {
-            controller.onBackPressed();
-          },
-          title: controller.toolbarTitle.value,
-          isHasTrailing: controller.toolbarTitle.value == LocaleKeys.jobs.tr,
-          trailingIcon: Icons.calendar_month,
-          trailingWidgetColor: Colors.white,
-          onPressedTrailing: () {
-            onTrailingPressed!();
-          }),
+        showCustomBackgroundImage: showCustomBackgroundImage,
+        isHasLeading: true,
+        onPressedLeading: () {
+          controller.onBackPressed();
+        },
+        title: controller.toolbarTitle.value,
+      ),
     );
   }
 }

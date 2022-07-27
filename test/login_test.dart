d8742464d8ca10e0c906224eab2ff45598b5cae7
Devu Mani <devu.seeroo@gmail.com>
2022-07-27 10:46:50 +0530

    api mock implementation research

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,6 +1,11 @@
+import 'package:alhaddad_driver/app/api/services/api_client.dart';
 import 'package:alhaddad_driver/app/modules/login/controllers/login_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+// import 'package:mocktail/mocktail.dart';
 
+// class MockLoginClient extends Mock implements ApiClient {}
+@GenerateMocks([ApiClient])
 void main() {
   final login = LoginController();
 
@@ -11,6 +16,7 @@ void main() {
     test("mobile_number_validation", () {
       expect(login.isMobileNumberValid("value"), null);
     });
+
     test("mobile_number_validation", () {
       expect(login.isMobileNumberValid("1234"), null);
     });
